d1f4f4666b06e584ab935f24f72b521b397bc593
Xavier <xavier04.martinez@gmail.com>
2021-10-23 06:31:34 -0500

    Update ProxyParty.java

--- src/main/java/me/heyimblake/proxyparty/ProxyParty.java
+++ src/main/java/me/heyimblake/proxyparty/ProxyParty.java
@@ -102,7 +102,7 @@ public final class ProxyParty extends Plugin {
     public String translatePrefix(User user) {
         String prefix = user.getCachedData().getMetaData().getPrefix();
 
-        return (prefix != null ? ChatColor.translateAlternateColorCodes('&', prefix) : ChatColor.GRAY) + " " + user.getUsername();
+        return (prefix != null ? ChatColor.translateAlternateColorCodes('&', prefix) : ChatColor.GRAY) + " " + redisBungee.getUuidTranslator().getNameFromUuid(user.getUniqueId(), true);
     }
 
     public MongoModel getMongo() {
